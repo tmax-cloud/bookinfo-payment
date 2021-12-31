5a3f1d01a29566c5edafcea848d9d9797d61276b
hayeon <hayeona92@gmail.com>
2021-12-31 10:56:28 +0900

    [refactor] replace RestTemplate with WebClient

--- src/main/java/org/tmaxcloud/sample/msa/book/payment/PaymentApplication.java
+++ src/main/java/org/tmaxcloud/sample/msa/book/payment/PaymentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PaymentApplication {
@@ -13,11 +14,9 @@ public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
-    
+
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
+    public WebClient webClient(WebClient.Builder webClientBuilder) { return webClientBuilder.build();}
 
     @Bean
     public ModelMapper modelMapper() {
